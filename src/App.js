50a654daf20d7f678344481b9252f581a953538e
Vivekanand Kumar <mailofvivekanand@gmail.com>
2022-07-08 20:19:11 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Items from "./Items";
+const App = () => {
+  const [currentItem, setCurrentItem] = useState("");
+  const [items, setItems] = useState([]);
 
-function App() {
+  const handleChange = (e) => {
+    const text = e.target.value;
+    setCurrentItem(text);
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (currentItem.trim() === "") {
+      return alert("Enter a todo..");
+    }
+    const newItem = {
+      id: Date.now(),
+      text: currentItem,
+      completed: false,
+    };
+    setItems([...items, newItem]);
+    setCurrentItem("");
+  };
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos) {
+      setItems(todos);
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(items));
+  }, [items]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outer">
+      <div className="inner">
+        <h2>Todo App</h2>
+        <form className="input-wrapper">
+          <input value={currentItem} onChange={handleChange} />
+          <button type="submit" onClick={handleClick}>
+            Add
+          </button>
+        </form>
+        <div className="list-wrapper">
+          <Items listItem={items} updateItem={setItems} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
